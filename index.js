79820aba65789362ff557c576fcc4f5fc337fd1f
kamlesh-chandel <kamlesh@gkmit.co>
2025-11-12 13:24:39 +0530

    chore: initialize backend project with Express setup

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config({});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
+
+const corsOption = {
+    origin : 'http://localhost:5173',
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+}
+
+app.use(cors(corsOption));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`app is listenning at ${PORT}`);
+})
