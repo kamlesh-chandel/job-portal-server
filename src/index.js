2a83c3fe6946f11886fe6ffe6be004a58a5aa0c5
kamlesh-chandel <kamlesh@gkmit.co>
2025-11-15 21:41:08 +0530

    feat: add auth.routes.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import userRoute from './routes/user.routes.js'
+import authRoute from './routes/auth.routes.js'
 
 dotenv.config({});
 
@@ -25,7 +25,7 @@ app.use(cors(corsOption));
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1/user", userRoute);
+app.use("/api/v1/auth", authRoute);
 
 app.listen(PORT, () => {
   connectDB();
