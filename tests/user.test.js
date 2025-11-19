ad0ced95fb06dfcb28448e11b09d30ce070d69b1
kamlesh-chandel <kamlesh@gkmit.co>
2025-11-20 02:59:16 +0530

    testing ci-cd key

--- tests/user.test.js
+++ tests/user.test.js
@@ -13,6 +13,7 @@ import app from '../src/index.js';
 import User from '../src/models/user.model.js';
 import { generateAccessToken } from '../src/utils/token.js';
 
+// Mock Cloudinary
 vi.mock('../src/config/cloudinary.js', () => ({
   default: {
     uploader: {
